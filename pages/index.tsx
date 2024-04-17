3c58f07bce3a8dc7757b4429384bdf2f02b6d8db
Matteo <matteo.tomicic@bind.hr>
2024-04-17 11:33:13 +0200

    feat(i18n): fetch content based on language

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-	return <div>Klima Merks</div>;
+import { GetStaticPropsContext } from "next";
+
+import { ContentType, IPageProps } from "../src/content/Home/Home.types";
+
+export default function Home(pageProps: IPageProps) {
+	const { Hello } = pageProps;
+
+	return <div>{Hello}</div>;
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+	const content: unknown = await import(`../src/content/Home/${locale}.json`);
+
+	return {
+		props: {
+			...(content as ContentType)
+		}
+	};
 }
---
+++ src/content/Home/Home.types.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import hr from "./hr.json";
+
+export type ContentType = typeof hr;
+
+export interface IPageProps extends ContentType {}
