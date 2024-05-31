484196be5c29b8d9496b8f0dd949ae1a26cc79be
Matteo <tomicic.matteo7@gmail.com>
2024-05-31 19:53:49 +0200

    fix(logo cloud): preload logo cloud images

--- src/components/LogoClouds/index.tsx
+++ src/components/LogoClouds/index.tsx
@@ -20,7 +20,7 @@ function LogoClouds({ heading, logos }: ILogoClouds) {
 
 				<LogosWrapper>
 					{logos.map((logo) => (
-						<Image alt={logo.alt} className="logo" height={90} key={logo.key} src={logo.src} width={140} />
+						<Image alt={logo.alt} className="logo" height={90} key={logo.key} src={logo.src} width={140} priority />
 					))}
 				</LogosWrapper>
 			</Container>
