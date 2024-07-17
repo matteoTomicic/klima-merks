ab95c5179819467bff4349e75f7c28d77d873591
Matteo <tomicic.matteo7@gmail.com>
2024-07-17 17:27:31 +0200

    Update it.ts

--- src/content/PriceList/it.ts
+++ src/content/PriceList/it.ts
@@ -65,103 +65,103 @@ const content: IPriceListContentTypes = {
 					key: 1,
 					serviceNumber: 2,
 					serviceLabel: "Servizio annuale regolare di caldaie combinate e circo",
-					servicePrice: "€89.00"
+					servicePrice: "€90.00"
 				},
 				{
 					key: 2,
 					serviceNumber: 3,
 					serviceLabel: "Servizio annuale regolare di caldaie a condensazione",
-					servicePrice: "€99.00"
+					servicePrice: "€100.00"
 				},
 				{
 					key: 3,
 					serviceNumber: 4,
 					serviceLabel: "Servizio di caldaie combinate con decalcificazione",
-					servicePrice: "€119.00"
+					servicePrice: "€120.00"
 				},
 				{
 					key: 4,
 					serviceNumber: 5,
 					serviceLabel: "Servizio completo del dispositivo VUI con smontaggio (da minimo)",
-					servicePrice: "€395.00"
+					servicePrice: "€450.00"
 				},
 				{
 					key: 5,
 					serviceNumber: 6,
 					serviceLabel: "Servizio di caldaia a condensazione VSC",
-					servicePrice: "€133.00"
+					servicePrice: "€140.00"
 				},
 				{
 					key: 6,
 					serviceNumber: 7,
 					serviceLabel: "Servizio di caldaia a gas",
-					servicePrice: "€89.00"
+					servicePrice: "€90.00"
 				},
 				{
 					key: 7,
 					serviceNumber: 8,
 					serviceLabel: "Servizio di scaldabagno ad accumulo",
-					servicePrice: "€89.00"
+					servicePrice: "€90.00"
 				},
 				{
 					key: 8,
 					serviceNumber: 9,
 					serviceLabel: "Servizio di scaldabagno ad accumulo con decalcificazione",
-					servicePrice: "€149.00"
+					servicePrice: "€150.00"
 				},
 				{
 					key: 9,
 					serviceNumber: 10,
 					serviceLabel: "Servizio di scaldabagno istantaneo",
-					servicePrice: "€79.00"
+					servicePrice: "€80.00"
 				},
 				{
 					key: 10,
 					serviceNumber: 11,
 					serviceLabel: "Servizio di scaldabagno istantaneo con decalcificazione",
-					servicePrice: "€99.00"
+					servicePrice: "€100.00"
 				},
 				{
 					key: 11,
 					serviceNumber: 12,
 					serviceLabel: "Messa in servizio di dispositivi a gas convenzionali",
-					servicePrice: "€79.00"
+					servicePrice: "€120.00"
 				},
 				{
 					key: 12,
 					serviceNumber: 13,
 					serviceLabel: "Messa in servizio di caldaia combinata a condensazione",
-					servicePrice: "€89.00"
+					servicePrice: "€120.00"
 				},
 				{
 					key: 13,
 					serviceNumber: 14,
 					serviceLabel: "Messa in servizio di caldaia a condensazione (PEEC)",
-					servicePrice: "€109.00"
+					servicePrice: "€120.00"
 				},
 				{
 					key: 14,
 					serviceNumber: 15,
 					serviceLabel: "Messa in servizio di caldaia circo a condensazione",
-					servicePrice: "€109.00"
+					servicePrice: "€120.00"
 				},
 				{
 					key: 15,
 					serviceNumber: 16,
 					serviceLabel: "Programmazione di termostato digitale per ambiente",
-					servicePrice: "€55.00"
+					servicePrice: "€50.00"
 				},
 				{
 					key: 16,
 					serviceNumber: 17,
 					serviceLabel: "Programmazione di termostato WiFi per ambiente",
-					servicePrice: "€79.00"
+					servicePrice: "€80.00"
 				},
 				{
 					key: 17,
 					serviceNumber: 18,
 					serviceLabel: "Programmazione e indirizzamento dell'automazione",
-					servicePrice: "€99.00"
+					servicePrice: "€100.00"
 				}
 			]
 		},
